25e1b0016d59efff396417395d422bac9e13aa63
striker hunt <strikerhunt@strikers-iMac-Pro.local>
2023-01-08 15:08:57 -0300

    slide de projetos adicionado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 
 import RotateCardTecnology from "./sections/Tecnologies.js";
-
+import SlideNav from "./sections/slide/slide.js";
 
 
 
 
 
 RotateCardTecnology()
+const slide = new SlideNav('.slide', '.slide-wrapper');
+
+slide.init();
+
+
+slide.addControl();
